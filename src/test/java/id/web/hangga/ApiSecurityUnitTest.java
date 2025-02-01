24779722d8c27a4aad4dd66e248028b8cc6f5c0e
Hangga Aji Sayekti <bazeniancode@gmail.com>
2025-02-01 16:04:31 +0700

    add circleci config

--- src/test/java/id/web/hangga/ApiSecurityUnitTest.java
+++ src/test/java/id/web/hangga/ApiSecurityUnitTest.java
@@ -1,6 +1,14 @@
 package id.web.hangga;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
+import static com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,26 +19,28 @@ import org.junit.jupiter.api.Test;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
 
 import io.restassured.http.ContentType;
 
 public class ApiSecurityUnitTest {
 
     private static WireMockServer wireMockServer;
+
     @BeforeAll
     public static void setUp() {
         wireMockServer = new WireMockServer(8080);
         wireMockServer.start();
         configureFor("localhost", 8080);
 
-        MappingBuilder loginRequest = post(urlPathEqualTo("/auth/login"))
-            .withHeader("Content-Type", containing("application/json"))
+        UrlPattern loginUrl = urlPathEqualTo("/auth/login");
+
+        MappingBuilder loginRequest = post(loginUrl).withHeader("Content-Type", containing("application/json"))
             .withRequestBody(matchingJsonPath("$.username"))
             .withRequestBody(matchingJsonPath("$.password"));
 
         // Login endpoint
-        stubFor(post(urlPathEqualTo("/auth/login"))
-            .withHeader("Content-Type", containing("application/json"))
+        stubFor(post(loginUrl).withHeader("Content-Type", containing("application/json"))
             .withRequestBody(matchingJsonPath("$.username"))
             .withRequestBody(matchingJsonPath("$.password"))
             .willReturn(aResponse().withStatus(200)
@@ -38,19 +48,18 @@ public class ApiSecurityUnitTest {
                 .withBody("{ \"token\": \"dummy-token\" }")));
 
         // Brute force protection
-        stubFor(post(urlPathEqualTo("/auth/login"))
-            .inScenario("Brute Force")
+        stubFor(post(loginUrl).inScenario("Brute Force")
             .whenScenarioStateIs(STARTED)
             .willReturn(aResponse().withStatus(401)));
 
         // SQL Injection attempt response
-        stubFor(post(urlPathEqualTo("/auth/login"))
-            .withRequestBody(containing("' OR '1'='1"))
+        stubFor(post(loginUrl).withRequestBody(containing("' OR '1'='1"))
             .willReturn(aResponse().withStatus(401)));
 
+        UrlPattern signupUrl = urlPathEqualTo("/auth/signup");
+
         // Sign-up endpoint - Success
-        stubFor(post(urlPathEqualTo("/auth/signup"))
-            .withHeader("Content-Type", containing("application/json"))
+        stubFor(post(signupUrl).withHeader("Content-Type", containing("application/json"))
             .withRequestBody(matchingJsonPath("$.username"))
             .withRequestBody(matchingJsonPath("$.password"))
             .withRequestBody(matchingJsonPath("$.email"))
@@ -58,18 +67,15 @@ public class ApiSecurityUnitTest {
                 .withHeader("Content-Type", "application/json")
                 .withBody("{ \"message\": \"User created successfully\" }")));
 
-
         // Sign-up endpoint - Username already exists (Error case)
-        stubFor(post(urlPathEqualTo("/auth/signup"))
-            .withHeader("Content-Type", containing("application/json"))
+        stubFor(post(signupUrl).withHeader("Content-Type", containing("application/json"))
             .withRequestBody(equalToJson("{\"username\": \"existinguser\", \"password\": \"password123\", \"email\": \"existing@example.com\"}"))
             .willReturn(aResponse().withStatus(409)
                 .withHeader("Content-Type", "application/json")
                 .withBody("{ \"message\": \"Username already exists\" }")));
 
         // Invalid email format response
-        stubFor(post(urlPathEqualTo("/auth/signup"))
-            .withRequestBody(matchingJsonPath("$.email", matching("^((?!@).)*$")))
+        stubFor(post(signupUrl).withRequestBody(matchingJsonPath("$.email", matching("^((?!@).)*$")))
             .willReturn(aResponse().withStatus(400)));
     }
 
@@ -80,8 +86,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testSignUpSuccess() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"newuser\", \"password\": \"password123\", \"email\": \"newuser@example.com\" }")
             .when()
             .post("http://localhost:8080/auth/signup")
@@ -92,8 +97,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testSignUpUsernameExists() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"existinguser\", \"password\": \"password123\", \"email\": \"existing@example.com\" }")
             .when()
             .post("http://localhost:8080/auth/signup")
@@ -104,8 +108,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testLoginInvalidCredentials() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"wronguser\", \"password\": \"wrongpass\" }")
             .when()
             .post("http://localhost:8080/auth/login")
@@ -115,8 +118,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testLoginMissingFields() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"user\" }")
             .when()
             .post("http://localhost:8080/auth/login")
@@ -126,8 +128,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testLoginSQLInjectionAttempt() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"admin\", \"password\": \"' OR '1'='1\" }")
             .when()
             .post("http://localhost:8080/auth/login")
@@ -137,8 +138,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testSignUpInvalidEmail() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"user123\", \"password\": \"pass123\", \"email\": \"invalid-email\" }")
             .when()
             .post("http://localhost:8080/auth/signup")
@@ -148,8 +148,7 @@ public class ApiSecurityUnitTest {
 
     @Test
     public void testSignUpMissingFields() {
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"newuser\", \"password\": \"password123\" }")
             .when()
             .post("http://localhost:8080/auth/signup")
@@ -160,15 +159,13 @@ public class ApiSecurityUnitTest {
     @Test
     public void testBruteForceProtection() {
         for (int i = 0; i < 5; i++) {
-            given()
-                .contentType(ContentType.JSON)
+            given().contentType(ContentType.JSON)
                 .body("{ \"username\": \"user\", \"password\": \"wrongpass\" }")
                 .when()
                 .post("http://localhost:8080/auth/login");
         }
 
-        given()
-            .contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
             .body("{ \"username\": \"user\", \"password\": \"wrongpass\" }")
             .when()
             .post("http://localhost:8080/auth/login")
