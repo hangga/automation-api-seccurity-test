fa3591f5610eba11719df4491edb74a0a6359494
Hangga Aji Sayekti <bazeniancode@gmail.com>
2025-02-22 21:07:03 +0700

    create ApiSecurityUnitTest class

--- src/test/java/id/web/hangga/ApiSecurityUnitTest.java
+++ src/test/java/id/web/hangga/ApiSecurityUnitTest.java
@@ -1,27 +1,6 @@
 package id.web.hangga;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
-import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
 public class ApiSecurityUnitTest {
-
+    public static void main(String[] args) {
+    }
 }
